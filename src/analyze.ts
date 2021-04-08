682a57c26e267534f58507935401ff3cece0c5ad
Phong Nguyen <phongnn@gmail.com>
2021-04-08 16:41:27 +0800

    Add processPeriod()

--- src/analyze.ts
+++ src/analyze.ts
@@ -10,12 +10,31 @@ async function processMonth(monthId: number) {
   console.log(result)
 }
 
+async function processPeriod(period: string) {
+  const result = await got
+    .post(`${config.apiBaseUrl}/analytics?period=${period}`)
+    .json()
+
+  console.log(result)
+}
+
+// async function processTest() {
+//   const result = await got.post(`${config.apiBaseUrl}/test`).json()
+//   console.log(result)
+// }
+
 function main(...args: string[]) {
   const [durationType, durationId] = args
   switch (durationType) {
     case "-m":
       processMonth(parseInt(durationId)).catch(console.error)
       break
+    case "-p":
+      processPeriod(durationId).catch(console.error)
+      break
+    // case "-t":
+    //   processTest().catch(console.error)
+    //   break
     default:
       console.log("Invalid CLI arguments.")
   }
