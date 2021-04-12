7b4172bd8ee90453f68a9b3f9fbae293e7501654
Phong Nguyen <phongnn@gmail.com>
2021-04-12 23:24:28 +0800

    Clean up data for month & period

--- src/clean.ts
+++ src/clean.ts
@@ -4,14 +4,29 @@ import config from "./config"
 
 async function removeTrip(tripId: string) {
   console.log(`Removing data for trip ${tripId}...`)
-  const result = await got.delete(`${config.apiBaseUrl}/trip/${tripId}`).json()
-  console.log(result)
+  await got.delete(`${config.apiBaseUrl}/trip?trip=${tripId}`)
+}
+
+async function removeMonth(monthId: string) {
+  console.log(`Removing data for month ${monthId}...`)
+  await got.delete(`${config.apiBaseUrl}/trip?month=${monthId}`)
+}
+
+async function removePeriod(period: string) {
+  console.log(`Removing data for period ${period}...`)
+  await got.delete(`${config.apiBaseUrl}/trip?period=${period}`)
 }
 
 function main(...args: string[]) {
   switch (args[0]) {
     case "-t":
       removeTrip(args[1]).catch(console.error)
+      break
+    case "-m":
+      removeMonth(args[1]).catch(console.error)
+      break
+    case "-p":
+      removePeriod(args[1]).catch(console.error)
       break
     default:
       console.log("Invalid CLI arguments.")
